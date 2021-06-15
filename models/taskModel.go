768eaacb898861d8f85932386f83f484e6e048c0
mcereal <mpbcurtis@gmail.com>
2021-06-15 13:11:14 -0400

    updating task model

---
+++ models/taskModel.go
@@ -0,0 +1,20 @@
+package models
+
+import (
+	"gorm.io/gorm"
+)
+
+type Tasks struct {
+	gorm.Model
+	USER_ID                string `json:"userId"`
+	TASK_NAME              string `json:"taskName"`
+	ESTIMATED_POMS         string `json:"estimatedPoms"`
+	POM_SECONDS            string `json:"pomSeconds"`
+	BREAK_SECONDS          string `json:"breakSeconds"`
+	COMPLETED_POMS         string `json:"completedPoms"`
+	COMPLETED_SMALL_BREAKS string `json:"completedSmallBreaks"`
+	COMPLETED_BIG_BREAKS   string `json:"completedBigBreaks"`
+	IS_COMPLETE            string `json:"isComplete"`
+	START_DATE             string `json:"startDate"`
+	COMPLETE_DATE          string `json:"completeDate"`
+}
