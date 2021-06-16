c156cfba25552b35c4c6d4eabb5e8673087297d3
Michael Curtis <mpbcurtis@gmail.com>
2021-06-16 18:48:36 -0400

    adding defaults

--- models/taskModel.go
+++ models/taskModel.go
@@ -7,14 +7,14 @@ import (
 type Tasks struct {
 	gorm.Model
 	USER_ID                uint
-	TASK_NAME              string `json:"taskName"`
-	ESTIMATED_POMS         string `json:"estimatedPoms"`
-	POM_SECONDS            string `json:"pomSeconds"`
-	BREAK_SECONDS          string `json:"breakSeconds"`
-	COMPLETED_POMS         string `json:"completedPoms"`
-	COMPLETED_SMALL_BREAKS string `json:"completedSmallBreaks"`
-	COMPLETED_BIG_BREAKS   string `json:"completedBigBreaks"`
-	IS_COMPLETE            string `json:"isComplete"`
-	START_DATE             string `json:"startDate"`
-	COMPLETE_DATE          string `json:"completeDate"`
+	TASK_NAME              string `gorm:"type:varchar(255); not null" json:"taskName"`
+	ESTIMATED_POMS         string `gorm:"type:varchar(255); not null" json:"estimatedPoms"`
+	POM_SECONDS            string `gorm:"type:int; default: 0" json:"pomSeconds"`
+	BREAK_SECONDS          string `gorm:"type:int; default: 0" json:"breakSeconds"`
+	COMPLETED_POMS         string `gorm:"type:int; default: 0" json:"completedPoms"`
+	COMPLETED_SMALL_BREAKS string `gorm:"type:int; default: 0" json:"completedSmallBreaks"`
+	COMPLETED_BIG_BREAKS   string `gorm:"type:int; default: 0" json:"completedBigBreaks"`
+	IS_COMPLETE            string `gorm:"type:boolean; default: false" json:"isComplete"`
+	START_DATE             string `gorm:"type:date;" json:"startDate"`
+	COMPLETE_DATE          string `gorm:"type:date;" json:"completeDate"`
 }
--- models/userModel.go
+++ models/userModel.go
@@ -6,19 +6,19 @@ import (
 
 type User struct {
 	gorm.Model
-	FIRST_NM                  string `json:"firstName"`
-	LAST_NM                   string `json:"lastName"`
-	EMAIL                     string `json:"email"`
-	PASSWORD                  string `json:"password"`
+	FIRST_NM                  string `gorm:"type:varchar(255); not null" json:"firstName"`
+	LAST_NM                   string `gorm:"type:varchar(255); not null" json:"lastName"`
+	EMAIL                     string `gorm:"type:varchar(255); not null" json:"email"`
+	PASSWORD                  string `gorm:"type:varchar(255); not null" json:"password"`
 	COUNTRY                   string `json:"country"`
 	ALIAS                     string `json:"alias"`
 	ALIAS_IMG                 string `json:"aliasImg"`
-	DISPLAY_PREF              string `json:"displayPref"`
-	TOTAL_POM_SECONDS         string `json:"totalPomSeconds"`
-	TOTAL_SMALL_BREAK_SECONDS string `json:"totalSmallBreakSeconds"`
-	TOTAL_BIG_BREAK_SECONDS   string `json:"totalBigBreakSeconds"`
-	POM_TIME                  string `json:"pomTime"`
-	SMALL_BREAK_TIME          string `json:"smallBreakTime"`
-	BIG_BREAK_TIME            string `json:"bigBreakTime"`
+	DISPLAY_PREF              string `gorm:"type:int; default: 1" json:"displayPref"`
+	TOTAL_POM_SECONDS         string `gorm:"type:int; default: 0" json:"totalPomSeconds"`
+	TOTAL_SMALL_BREAK_SECONDS string `gorm:"type:int; default: 0" json:"totalSmallBreakSeconds"`
+	TOTAL_BIG_BREAK_SECONDS   string `gorm:"type:int; default: 0" json:"totalBigBreakSeconds"`
+	POM_TIME                  string `gorm:"type:int; default: 25" json:"pomTime"`
+	SMALL_BREAK_TIME          string `gorm:"type:int; default: 5" json:"smallBreakTime"`
+	BIG_BREAK_TIME            string `gorm:"type:int; default: 15" json:"bigBreakTime"`
 	Tasks                     []Tasks
 }
